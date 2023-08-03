d2c113966f4b201a6b955d472945623a6fb48540
parihar23 <parihar.ravindra23@mail.com>
2023-08-03 21:02:46 +0530

    add category file

---
+++ resources/views/addCategory.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+@section('content')
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+@php $id = $name = ''; @endphp
+@if($categories!='')
+   @php 
+    $id = $categories->id;
+    $name = $categories->category_name; 
+   @endphp 
+@endif
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">@if($categories!=''){{'Edit'}}@else{{'Add'}}@endif Category</div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <form method="POST" action="{{ route('addCategory') }}">
+                        @csrf
+                        <input type="hidden" name="category_id" value="{{$id}}" />
+                        <div class="form-group row">
+                            <label for="category" class="col-md-4 col-form-label text-md-right">{{ __('Category') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="category" type="text" class="form-control @error('category') is-invalid @enderror" name="category" value="@if(old('category')!=''){{old('category')}}@else{{$name}}@endif" required autocomplete="category" autofocus>
+
+                                @error('category')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    @if($categories!=''){{'Edit'}}@else{{'Add'}}@endif
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
